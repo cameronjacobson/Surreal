1f9f2f67a2214e16b00876d5a8d56acf1bc3cc50
Cameron Jacobson <cameron@datashovel.com>
2015-04-17 11:39:38 +0000

    added to example

--- examples/example.php
+++ examples/example.php
@@ -14,11 +14,21 @@ class Blah
 	protected static $blah5 = 5;
 	private static $blah6 = 6;
 	public $blah7;
+
+	public function abc(){
+		var_dump(self::$blah5);
+		var_dump(self::$blah6);
+	}
+	public function setit(){
+		self::$blah5 = 99;
+		self::$blah6 = 101;
+	}
 }
 
 $blah = new Blah();
 $blah->blah4 = 33;
 $blah->blah7 = new Blah();
+$blah->setit();
 
 $values = array(
 	1,
@@ -37,15 +47,13 @@ foreach($values as $key => $value){
 	echo 'TEST#: '.$key.PHP_EOL;
 
 	$start = microtime(true);
-	$ser = Surreal::surrealize($value);
-	echo ' '.(microtime(true) - $start).PHP_EOL;
-
-	$start = microtime(true);
 	$ser = serialize($value);
 	echo ' '.(microtime(true) - $start).PHP_EOL;
 
-	//echo $ser.PHP_EOL;
+	$start = microtime(true);
+	$ser = Surreal::surrealize($value);
+	echo ' '.(microtime(true) - $start).PHP_EOL;
+var_dump($ser);
 	var_dump($gg = unserialize($ser));
 }
-
-//var_dump($gg::$blah4);
+$gg->abc();
